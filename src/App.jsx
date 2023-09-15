81a0a23249556f3a89b38520c746d9b6a52f82fb
larcondo <larcondo90@gmail.com>
2023-09-14 23:22:35 -0300

    exercise 8.8
    create Authors view to show all authors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState('')
 
   return (
     <div>
-      <h1>Frontend - Library Project</h1>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} />
+
+      <Books show={page === 'books'} />
+      
+      <NewBook show={page === 'add'} />
+
     </div>
   )
 }
---
+++ src/components/Books.jsx
@@ -0,0 +1,30 @@
+const Books = (props) => {
+  if (!props.show) return null
+
+  const books = []
+
+  return(
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          { books.map( b => (
+            <tr key={b.title}>
+              <td>{ b.title }</td>
+              <td>{ b.author }</td>
+              <td>{ b.published }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
